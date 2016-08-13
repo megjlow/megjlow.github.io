0b337e47d4bde71cd08764b36eb7fe7c66c71497
jaj <jaj@darkblack.org>
2016-08-13 12:17:37 +0100

    Added extension loader, can't pass parameters to scratchx extension loader due to security exception

---
+++ extensionloader.js
@@ -0,0 +1,11 @@
+new (function() {
+	var ext = this;
+	
+	var descriptor = {
+    blocks: [
+      [' ', 'Load extension block %s', 'block', '1', 'off']
+    ],
+    url: 'http://www.warwick.ac.uk/tilesfortales'
+  };
+	
+});
