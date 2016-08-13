012fbaa046233370b90567a420b89b8ce208c2f8
jaj <jaj@darkblack.org>
2016-08-13 12:23:41 +0100

    Implement the API

--- extensionloader.js
+++ extensionloader.js
@@ -2,11 +2,18 @@ new (function() {
 	var ext = this;
 	
 	var descriptor = {
-    blocks: [
-      [' ', 'Load extension block %s', 'block', 'url'],
-      ['', 'speak %s', 'speak', "Hello!"],
-    ],
-    url: 'http://www.warwick.ac.uk/tilesfortales'
-  };
+    	blocks: [
+      		[' ', 'Load extension block %s', 'block', 'url'],
+    	],
+    	url: 'http://www.warwick.ac.uk/tilesfortales'
+  	};
+  
+  	ext._shutdown = function() {};
+  	
+  	ext._getStatus = function() {
+  		return {status: 2, msg: 'Device connected'}
+  	};
+  	
+  	ScratchExtensions.register("extensionloader", descriptor, ext);
 	
 });
