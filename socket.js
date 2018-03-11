c0f07521ca36d6a76e24313399e0fe2291b812fc
james-johnston <jaj@darkblack.org>
2018-03-11 14:13:15 +0000

    Update socket.js

--- socket.js
+++ socket.js
@@ -71,6 +71,7 @@ new (function() {
 			[' ', ext.ip + ': connect', 'connect'],
 			['r', ext.ip + ': isConnected', 'isConnected'],
 			[' ', ext.ip + ': disconnect', 'disconnect'],
+			['h', ext.ip + ': isConnected', 'isConnected'],
 	      		[' ', ext.ip + ': digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
 	      		[' ', ext.ip + ': pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
 	      		[' ', ext.ip + ': digital pin %m.pin get', 'getDigital', '1'],
@@ -137,7 +138,6 @@ new (function() {
   }
   
   ext._getStatus = function() {
-    console.log("getStatus");
     var retval = {status: 1, msg: 'Not Connected'};
     if(ext.socket != null && ext.socket.readyState == ext.socket.OPEN) {
       retval = {status: 2, msg: 'Device connected'};
