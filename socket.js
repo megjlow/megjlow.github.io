a2fcf618a41f04add56db51831b00796be4cad59
james-johnston <jaj@darkblack.org>
2018-03-11 14:48:18 +0000

    Update socket.js

--- socket.js
+++ socket.js
@@ -71,7 +71,7 @@ new (function() {
 			[' ', ext.ip + ': connect', 'connect'],
 			[' ', ext.ip + ': disconnect', 'disconnect'],
 			['b', ext.ip + ': isConnected', 'isConnected'],
-			[' ', ext.ip + ': setPinMode %m.pin %m.ioMode', 'setPinMode', 'setPinMode', 0, 'output'],
+			[' ', ext.ip + ': setPinMode %m.pin %m.ioMode', 'setPinMode', 0, 'output'],
 	      		[' ', ext.ip + ': digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
 	      		[' ', ext.ip + ': pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
 	      		[' ', ext.ip + ': digital pin %m.pin get', 'getDigital', '1'],
