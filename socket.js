f4fbda79ff5b05f77ba68ab861af999c43f6352a
jaj <jaj@darkblack.org>
2018-04-04 13:39:03 +0100

    Remove url decode

--- socket.js
+++ socket.js
@@ -112,7 +112,7 @@ new (function() {
   
   ext.onMessage = function(evt)
   {
-    console.log("Received: " + decodeURIComponent(evt.data));
+    console.log("Received: " + evt.data);
   }
   
   ext.doSend = function(message)
