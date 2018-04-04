d969c58876a1a6c68009aca9d5f99a3f8bc6388e
james-johnston <jaj@darkblack.org>
2018-04-04 11:07:23 +0000

    Create load.js

---
+++ load.js
@@ -0,0 +1,29 @@
+/*
+For testing, saves having to load extenions every page refresh
+*/
+
+new (function() {
+	var ext = this;
+	
+	var descriptor = {
+    	blocks: [
+      		[' ', 'Load extension block %s', 'loadBlock', 'url', 'url'],
+    	],
+    	url: 'http://www.warwick.ac.uk/tilesfortales'
+  	};
+  
+  	ext._shutdown = function() {};
+  	
+  	ext._getStatus = function() {
+  		return {status: 2, msg: 'Device connected'}
+  	};
+  	
+  	ext.loadBlock = function(url) {
+  		ScratchExtensions.loadExternalJS(url);
+  	};
+  	
+  	ScratchExtensions.register("extensionloader", descriptor, ext);
+    
+    ext.loadBlock("https://megjlow.github.io/socket.js?ip=192.168.2.105");
+	
+});
