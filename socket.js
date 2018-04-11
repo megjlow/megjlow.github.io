16d4835dd534fbff9e9608a99e470e862acef2e1
jaj <jaj@darkblack.org>
2018-04-11 11:18:27 +0100

    Set digital value

--- socket.js
+++ socket.js
@@ -196,11 +196,13 @@ new (function() {
 	    if(setting == 'off') {
 	      s = 0;
 	    }
-	    var bytearray = new Uint8Array(4);
+	    var bytearray = new Uint8Array(3);
 	    bytearray[0] = 0xF5; // set digital pin value
 	    bytearray[1] = 2;
 	    bytearray[2] = s;
-	    bytearray[2] = 0;
+	    
+	    console.log("bytearray length: " + bytearray.length);
+	    
 	    ext.socket.send(bytearray.buffer);
 	    
 	    console.log("setDigital");
