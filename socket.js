836c4f291f9d31dda6bb9fdd41367ce668be05d5
jaj <jaj@darkblack.org>
2018-05-28 11:57:47 +0100

    reportDigital

--- socket.js
+++ socket.js
@@ -77,7 +77,7 @@ new (function() {
 	      		[' ', ext.ip + ': pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
 	      		['R', ext.ip + ': digital pin %m.pin get', 'getDigital', '1'],
 	      		[' ', ext.ip + ': pwm pin %m.ppin get', 'getPwm', '1'],
-	      		[' ', ext.ip + ': report digital callback %m.port %m.enableDisable', 'reportDigital', '1', 'enable'],
+	      		[' ', ext.ip + ': report digital callback %m.pin %m.enableDisable', 'reportDigital', '1', 'enable'],
 	    	],
 	    	'menus': {
 	      		'pin': ['12', '2', '3'],
