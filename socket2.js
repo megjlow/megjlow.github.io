6cb2a902c1d1ea354b1197eced06a0543ed417c7
jaj <jaj@darkblack.org>
2018-07-01 14:37:08 +0100

    connect

--- socket2.js
+++ socket2.js
@@ -1,5 +1,6 @@
 new (function() {
 	var ext = this;
+	var ext.socket = null;
   
   var getUrlParameter = function getUrlParameter(sParam) {
 	    var sPageURL = decodeURIComponent(document.currentScript.src.split("?")[1]),
@@ -34,22 +35,40 @@ new (function() {
 	console.log('ip ' + ext.ip);
 	console.log('using url ' + ext.url);
   
-  var descriptor = {
-    	blocks: [
-      		[' ', ext.name != null ? ext.name : ext.ip + ': digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
-      		[' ', ext.name != null ? ext.name : ext.ip + ': pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
-      		[' ', ext.name != null ? ext.name : ext.ip + ': digital pin %m.pin get', 'getDigital', '1'],
-      		[' ', ext.name != null ? ext.name : ext.ip + ': pwm pin %m.ppin get', 'getPwm', '1']
-    	],
-    	'menus': {
-      		'pin': ['1', '2', '3'],
-      		'dsetting': ['on', 'off'],
-      		'ppin': ['1', '2']
-     	},
-    	url: 'http://www.warwick.ac.uk/tilesfortales'
-  };
+	var descriptor = {
+		blocks: [
+			[' ', ext.name != null ? ext.name : ext.ip + ': connect', 'connect'],
+			[' ', ext.name != null ? ext.name : ext.ip + ': disconnect', 'disconnect'],
+			['b', ext.name != null ? ext.name : ext.ip + ': isConnected', 'isConnected'],
+			[' ', ext.name != null ? ext.name : ext.ip + ': digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
+			[' ', ext.name != null ? ext.name : ext.ip + ': pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
+			[' ', ext.name != null ? ext.name : ext.ip + ': digital pin %m.pin get', 'getDigital', '1'],
+			[' ', ext.name != null ? ext.name : ext.ip + ': pwm pin %m.ppin get', 'getPwm', '1']
+		],
+		'menus': {
+		'pin': ['1', '2', '3'],
+		'dsetting': ['on', 'off'],
+		'ppin': ['1', '2']
+		},
+		url: 'http://www.warwick.ac.uk/tilesfortales'
+	};
 	
-ext.isConnected = function() {
+	ext.connect = function() {
+		if(ext.socket == null) {
+			ext.socket = new WebSocket("ws://" + ext.ip, 'firmata');
+			ext.socket.binaryType = "arraybuffer";
+			ext.socket.onopen = function(evt) {ext.onOpen(evt)};
+			ext.socket.onmessage = function(evt) {ext.onMessage(evt)};
+			ext.socket.onclose = function(evt) {ext.onClose(evt)};
+		}
+		else if(ext.socket.readyState == ext.socket.CLOSING || ext.socket.readyState == ext.socket.CLOSED) {
+			ext.socket = null;
+			ext.connect();
+		}
+		// if socket is in open or connecting state we're not going to do anything
+	}
+	
+	ext.isConnected = function() {
 		var retval = false;
 		if(ext.socket != null && ext.socket.readyState == ext.socket.OPEN) {
 			retval = true;
