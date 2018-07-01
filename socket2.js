a9446a2034d40a4dba567edd2d17a0f339d08c3f
jaj <jaj@darkblack.org>
2018-07-01 15:29:28 +0100

    connect

--- socket2.js
+++ socket2.js
@@ -1,42 +1,4 @@
-new (function() {
-	var ext = this;
-	ext.socket = null;
-	ext.board = null;
-  
-  var getUrlParameter = function getUrlParameter(sParam) {
-	    var sPageURL = decodeURIComponent(document.currentScript.src.split("?")[1]),
-	        sURLVariables = sPageURL.split('&'),
-	        sParameterName,
-	        i;
-	
-	    for (i = 0; i < sURLVariables.length; i++) {
-	        sParameterName = sURLVariables[i].split('=');
-	
-	        if (sParameterName[0] === sParam) {
-	            return sParameterName[1] === undefined ? true : sParameterName[1];
-	        }
-	    }
-	};
-
-	ext.name = getUrlParameter('name');
-	ext.ip = getUrlParameter('ip');
-	
-	if(ext.name != undefined) {
-		// we'll use the name supplied by preference
-		ext.url = "http://" + ext.name;
-		console.log('got name');
-	}
-	else {
-		// no name supplied so use the IP address
-		ext.url = "http://" + ext.ip;
-		console.log("didn't get name");
-	}
-	
-	console.log('name ' + ext.name);
-	console.log('ip ' + ext.ip);
-	console.log('using url ' + ext.url);
-	
-	// firmata.js
+// firmata.js
 	
 	"use strict";
 
@@ -2724,6 +2686,44 @@ function decodeCustomFloat(input) {
 	
 	
 	//end firmata.js
+
+new (function() {
+	var ext = this;
+	ext.socket = null;
+	ext.board = null;
+  
+  var getUrlParameter = function getUrlParameter(sParam) {
+	    var sPageURL = decodeURIComponent(document.currentScript.src.split("?")[1]),
+	        sURLVariables = sPageURL.split('&'),
+	        sParameterName,
+	        i;
+	
+	    for (i = 0; i < sURLVariables.length; i++) {
+	        sParameterName = sURLVariables[i].split('=');
+	
+	        if (sParameterName[0] === sParam) {
+	            return sParameterName[1] === undefined ? true : sParameterName[1];
+	        }
+	    }
+	};
+
+	ext.name = getUrlParameter('name');
+	ext.ip = getUrlParameter('ip');
+	
+	if(ext.name != undefined) {
+		// we'll use the name supplied by preference
+		ext.url = "http://" + ext.name;
+		console.log('got name');
+	}
+	else {
+		// no name supplied so use the IP address
+		ext.url = "http://" + ext.ip;
+		console.log("didn't get name");
+	}
+	
+	console.log('name ' + ext.name);
+	console.log('ip ' + ext.ip);
+	console.log('using url ' + ext.url);
 	
 	// websocket event handlers
 	
