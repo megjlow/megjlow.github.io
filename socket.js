6c14c626bfbf9ced84ef5e1f5c7c5cd6b628db56
james-johnston <jaj@darkblack.org>
2018-07-24 22:26:39 +0100

    Don't run report version on connect

--- socket.js
+++ socket.js
@@ -908,7 +908,7 @@ function Board(port, options, callback) {
     clearTimeout(this.reportVersionTimeoutId);
     this.versionReceived = true;
     //this.one("queryfirmware", function() {
-
+/*
       // Only preemptively set the sampling interval if `samplingInterval`
       // property was _explicitly_ set as a constructor option.
       if (options.samplingInterval !== undefined) {
@@ -926,6 +926,7 @@ function Board(port, options, callback) {
             this.pins.push({supportedModes: [], analogChannel: analogChannel});
           }
         }
+*/
 
         // If the capabilities query is skipped,
         // default resolution values will be used.
